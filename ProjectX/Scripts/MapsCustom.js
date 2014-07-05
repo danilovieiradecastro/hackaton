ce815083e74b6221efd3ede91e296ebe8536a425
ramonzimcs <ramonscampos@gmail.com>
2014-07-05 16:34:40 -0300

    searchBar

--- ProjectX/Scripts/MapsCustom.js
+++ ProjectX/Scripts/MapsCustom.js
@@ -1,11 +1,12 @@
 ﻿$(document).ready(function () { 
+    var teste;
     var map;
     function initialize() {
         var mapOptions = {
             zoom: 15,
             center: new google.maps.LatLng(-34.397, 150.644)
         };
-        map = new google.maps.Map(document.getElementById('bodyContent'), mapOptions);
+        map = new google.maps.Map(document.getElementById('Map'), mapOptions);
         // Try HTML5 geolocation
         if(navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(function(position) {
@@ -21,11 +22,25 @@
             }, function() {
                 handleNoGeolocation(true);
             });
+
         } else {
             // Browser doesn't support Geolocation
             handleNoGeolocation(false);
         }
+
+        //google.maps.event.addListener(map, "tilesloaded", function () {
+        //    teste = $(".gm-style > div");
+            
+        //    $(teste[7]).css("top","50px");
+            
+            
+            
+        //});
+
     }
 
     google.maps.event.addDomListener(window, 'load', initialize);
+
+    
+
 });
