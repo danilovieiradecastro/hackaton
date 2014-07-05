292ab8b28e88519188ad46c60bd3ae63b0c2a43f
ramonzimcs <ramonscampos@gmail.com>
2014-07-05 19:27:40 -0300

    Track

--- ProjectX/Scripts/MainCustom.js
+++ ProjectX/Scripts/MainCustom.js
@@ -1,21 +1,34 @@
 ﻿$(document).ready(function () {
+    
     $("#btnDescribePlace").click(function () {
-        
+        var classe = "";
         $.post("/DescribePlace/RetornarViewAjax")
                 .done(function (data) {
                     $("#overlay").show();
                     $("#ModalContent").html(data);
+                    $("#modal").addClass(classe);
+                    $("body").css("overflow", "hidden");
                 });
-
         return false;
     });
 
-
+    $("#btnPosts").click(function () {
+        var classe = "modalPosts";
+        $.post("/Posts/RetornarViewAjax")
+                .done(function (data) {
+                    $("#overlay").show();
+                    $("#ModalContent").html(data);
+                    $("#modal").addClass(classe);
+                    $("body").css("overflow", "hidden");
+                });
+        return false;
+    });
 
 });
 
 
 function closeModal() {
     $("#overlay").hide();
-    $("#ModalContent").hide();
+    $("#ModalContent").html("");
+    $("body").css("overflow", "hidden");
 }
