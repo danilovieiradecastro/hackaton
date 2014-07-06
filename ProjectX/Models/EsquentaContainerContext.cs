57c8ff56edf458a8347736ab67ec090c10d6d02c
danilovieiradecastro <dvcastro@hotmail.com>
2014-07-06 01:56:23 -0300

    mudanca no modelo

--- ProjectX/Models/EsquentaContainerContext.cs
+++ ProjectX/Models/EsquentaContainerContext.cs
@@ -18,11 +18,13 @@ namespace ProjectX.Models
 
         public DbSet<LocalSet> LocalSets { get; set; }
         public DbSet<PostSet> PostSets { get; set; }
+        public DbSet<UserSet> UserSets { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Configurations.Add(new LocalSetMap());
             modelBuilder.Configurations.Add(new PostSetMap());
+            modelBuilder.Configurations.Add(new UserSetMap());
         }
     }
 }
---
+++ ProjectX/Models/Mapping/UserSetMap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+
+namespace ProjectX.Models.Mapping
+{
+  public class UserSetMap: EntityTypeConfiguration<UserSet>
+  {
+    public UserSetMap()
+    {
+      // Primary Key
+      this.HasKey(t => t.Id);
+
+      // Properties
+
+      this.ToTable("UserSet");
+      this.Property(t => t.Id).HasColumnName("Id");
+      this.Property(t => t.Email).HasColumnName("Email");
+      this.Property(t => t.Nome).HasColumnName("Nome");
+    }  
+  }
+}
