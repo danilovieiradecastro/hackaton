6d3abde45cd671803a29827031233bdecc263139
ramonzimcs <ramonscampos@gmail.com>
2014-07-05 23:07:28 -0300

    Correção commit

--- ProjectX/Scripts/TrackBarQuality.js
+++ ProjectX/Scripts/TrackBarQuality.js
@@ -1,21 +1,14 @@
-﻿var custQuality = document.querySelector('.sliderQualityCustomized');
-var initCust = new Powerange(custQuality, { hideRange: true, callback: changeQualityImage });
+﻿$(document).ready(function () {
 
-function changeQualityImage() {
-    var changeInput = document.querySelector('.js-check-change');
-    var imgSrc = "";
-    var imgObj = document.getElementById("qualityImage");
+    $("#slider").slider({
+        value: 20,
+        min: 20,
+        max: 100,
+        step: 20,
+        slide: function (event, ui) {
+            $("#amount").val(ui.value);
+        }
+    });
+    $("#amount").val($("#slider").slider("value"));
 
-    if (changeInput < 20)
-        imgObj.src = '/Images/capeta.png';
-    else if (changeInput > 20 && changeInput < 40)
-        imgObj.src = '/Images/pega bebado.png';
-    else if (changeInput > 40 && changeInput < 60)
-        imgObj.src = '/Images/pegavel.png';
-    else if (changeInput > 60 && changeInput < 80)
-        imgObj.src = '/Images/bonita.png';
-    else if (changeInput > 80)
-        imgObj.src = '/Images/princesa.png';
-    //alert('ok');
-    // document.getElementById('js-display-decimal').innerHTML = dec.value;
-}
+});
